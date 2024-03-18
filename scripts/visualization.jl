1913c32d5b56e5a849a99cbd216200f7110b44b5
Tobias Blickhan <tobias.blickhan@gmail.com>
2024-03-18 07:37:13 -0400

    some rank diagnistics for visualization

--- scripts/visualization.jl
+++ scripts/visualization.jl
@@ -2,9 +2,10 @@ using HDF5
 using Plots
 using LaTeXStrings
 using LinearAlgebra
+using BrenierTwoFluid
 
 ### Input the .hdf5 here
-results = "runs/2024-03-06T18:51:21.296.hdf5"
+results = "runs/2024-03-15T12:29:44.698.hdf5"
 ###
 
 # read results and parameters
@@ -20,23 +21,28 @@ tol =     read(fid["tol"])
 crit_it = read(fid["crit_it"])
 p_ω =     read(fid["p"])
 Δt =      read(fid["deltat"])
-α =       read(fid["alpha"])
+sol_α =       read(fid["alpha"])
 β =       read(fid["beta"])
+sol_species = read(fid["species"])
 close(fid)
 nt = size(solX,1) - 1
 N = size(solX,2)
 
-λ²
-N
-ε
-tol
-Δt
-crit_it
-p_ω
+    # colors
+    species = zero(α)
+    for i in 1:N
+        if X[i,1] < -1/6
+            species[i] = 1
+        elseif X[i,1] > 1/6
+            species[i] = 3
+        else
+            species[i] = 2
+        end
+    end
 
 
 # calculate diagnostics
-K = 1/2 * [dot(solV[i,:,:], diagm(α) * solV[i,:,:]) for i in axes(solV,1)]; #kinetic energy
+K = 1/2 * [dot(solV[i,:,:], diagm(sol_α[i,:]) * solV[i,:,:]) for i in axes(solV,1)]; #kinetic energy
 
 # energy Plot
 plot(K .- K[1], minorgrid = true, xlabel = L"t",
@@ -50,12 +56,7 @@ savefig("figs/energy.pdf")
 
 # Particle plots
 for j in axes(solX,1)
-    plt = scatter(solX[j,1:div(N,3),1], solX[j,1:div(N,3),2], label = false, color = palette(:default)[1], markerstrokewidth=0, markersize = 2.5,
-                legendfontsize=14, tickfontsize=10, xguidefontsize=14, yguidefontsize=14, xlabel = L"x_1", ylabel = L"x_2")
-    scatter!(solX[j,div(N,3)+1:div(2N,3),1], solX[j,div(N,3)+1:div(2N,3),2], label = false, 
-            color = palette(:default)[2], markerstrokewidth=0, markersize = 2.5)
-    scatter!(solX[j,div(2N,3)+1:end,1], solX[j,div(2N,3)+1:end,2], label = false, 
-    color = palette(:default)[3], markerstrokewidth=0, markersize = 2.5)
+    scatter(solX[j,:,1], solX[j,:,2], label = false, color = palette(:default)[Int.(sol_species[j,:])], markerstrokewidth=0, markersize = 2.5, legendfontsize=14, tickfontsize=10, xguidefontsize=14, yguidefontsize=14, xlabel = L"x_1", ylabel = L"x_2", xlim=(-0.55,0.55), ylim=(-0.55,0.55))
     savefig("figs/particles_$(j).pdf")
 end
 
@@ -71,16 +72,11 @@ plot(ΔV)
 savefig("figs/V_error.pdf")
 
 anim = @animate for j in axes(solX,1)
-    plt = scatter(solX[j,1:div(N,3),1], solX[j,1:div(N,3),2], label = false, color = palette(:default)[1], markerstrokewidth=0, markersize = 2.5,
-                xlims = (-0.55,0.55), ylims = (-0.55,0.55),
-                legendfontsize=14, tickfontsize=10, xguidefontsize=14, yguidefontsize=14, xlabel = L"x_1", ylabel = L"x_2")
-    scatter!(solX[j,div(N,3)+1:div(2N,3),1], solX[j,div(N,3)+1:div(2N,3),2], label = false, 
-            color = palette(:default)[2], markerstrokewidth=0, markersize = 2.5)
-    scatter!(solX[j,div(2N,3)+1:end,1], solX[j,div(2N,3)+1:end,2], label = false, 
-    color = palette(:default)[3], markerstrokewidth=0, markersize = 2.5)
+    scatter(solX[j,:,1], solX[j,:,2], label = false, color = palette(:default)[Int.(sol_species[j,:])], markerstrokewidth=0, markersize = 2.5, legendfontsize=14, tickfontsize=10, xguidefontsize=14, yguidefontsize=14, xlabel = L"x_1", ylabel = L"x_2", xlim=(-0.55,0.55), ylim=(-0.55,0.55))
 end
 gif(anim, "figs/euler.gif", fps = 8)
 
+#=
 massvec = []
 nomassvec = []
 for i in axes(solX,2)
@@ -95,3 +91,31 @@ anim = @animate for j in axes(solX,1)
     scatter!([solX[j,massvec,1]], [solX[j,massvec,2]], label = false, markerstrokewidth=0, markersize = 2.5, color = :red, xlims = (-0.55,0.55), ylims = (-0.55,0.55))
 end
 gif(anim, "figs/raleigh_taylor.gif", fps = 8)
+=#
+
+# Rank Stuff
+Y = stack(vec([ [x,y] for x in range(-0.5,0.5,length=Int(sqrt(M))), y in range(-0.5,0.5,length=Int(sqrt(M))) ]), dims = 1);
+Y .= Y[sortperm(Y[:,1]), :];
+
+plt = plot(xlabel = L"i", ylabel = L"\sigma_i")
+for j in axes(solX,1)
+    if j % 10 == 0 || j==1
+        CC = CostCollection(solX[j,:,:], Y, (x,y) -> 0.5 * sqeuclidean(x,y));
+        K = exp.(-CC.C_xy/ε)
+        svdK = svd(K);
+        plot!(svdK.S, yscale=:log10, label = "$j") 
+    end
+end
+plt
+
+#=
+p = 3
+h = sqrt(ε)
+s_vec = [1,2,3];
+ρ_vec = [ vec([ kde([x, y], solX[end,:,:], α, h, p, s, species) for x in range(-0.5,0.5,length=Int(sqrt(N))), y in range(-0.5,0.5,length=Int(sqrt(N)))]) for s in s_vec ]
+for ρ in ρ_vec
+    ρ ./= sum(ρ)
+    ρ ./= length(s_vec) # number of species
+end
+=#
+
